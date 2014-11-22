f9323b638bc33c3e7ff3e80a2d71313c085aa4d2
Tom Sabin <tom.sabin@unboxedconsulting.com>
2014-11-22 09:21:12 +0000

    Copy over existing landing page contents to welcome#home

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,5 @@
 //= require jquery
 //= require jquery_ujs
+//= require_tree ./jquery_modules
 //= require turbolinks
 //= require_tree .
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,4 @@
 class WelcomeController < ApplicationController
-  def index
+  def home
   end
 end
