0e0bcbaeb102c8acae363dbc3db5a331cba769d9
Tom Sabin <tom.sabin@unboxedconsulting.com>
2015-03-31 18:18:19 -0400

    When deleting a User, also delete their Profile

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe User do
   describe 'validations' do
-    it { should have_one(:profile) }
+    it { should have_one(:profile).dependent(:destroy) }
     it { should validate_presence_of(:first_name) }
     it { should validate_presence_of(:last_name) }
     it { should validate_presence_of(:email) }
