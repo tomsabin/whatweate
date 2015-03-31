94775f972b2a19c3bd5b5eb490a7f552291f8070
Tom Sabin <tom.sabin@unboxedconsulting.com>
2015-03-31 17:47:20 -0400

    Migration to add Profile model and association

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150326194905) do
+ActiveRecord::Schema.define(version: 20150326201240) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "profiles", force: true do |t|
+    t.date     "date_of_birth"
+    t.string   "profession"
+    t.string   "greeting"
+    t.text     "bio"
+    t.string   "mobile_number"
+    t.string   "favorite_cuisine"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "user_id"
+  end
+
+  add_index "profiles", ["user_id"], name: "index_profiles_on_user_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
