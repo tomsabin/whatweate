45dd02c2a240df7a3a125b2103f9aeac5dee1509
Tom Sabin <tom.sabin@unboxedconsulting.com>
2015-04-05 17:54:18 -0400

    Use _url in controllers instead of _path

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -7,15 +7,15 @@ class ProfilesController < ApplicationController
   end
 
   def new
-    redirect_to(profile_path) && return if profile.present?
+    redirect_to(profile_url) && return if profile.present?
     @profile = Profile.new
   end
 
   def create
-    @profile = Profile.new(profile_params)
-    @profile.user = current_user
-    if @profile.save
-      redirect_to(profile_path, notice: "Thanks! Your profile has successfully been saved")
+    @profile = Profile.new(user: current_user)
+    @profile.save(validate: false)
+    if @profile.update(profile_params.merge(user_attributes: user_params.merge(id: current_user.id)))
+      redirect_to(profile_url, notice: "Thanks! Your profile has successfully been saved")
     else
       flash.alert = "Please fill in the required fields"
       render(:new)
@@ -27,7 +27,7 @@ class ProfilesController < ApplicationController
 
   def update
     if profile.update(profile_params.merge(user_attributes: user_params.merge(id: current_user.id)))
-      redirect_to(profile_path, notice: "Your profile has successfully been saved")
+      redirect_to(profile_url, notice: "Your profile has successfully been saved")
     else
       render(:edit)
     end
@@ -36,7 +36,7 @@ class ProfilesController < ApplicationController
   private
 
   def complete_profile
-    redirect_to(new_profile_path, notice: "Please complete your profile") if profile.blank?
+    redirect_to(new_profile_url, notice: "Please complete your profile") if profile.blank?
   end
 
   def assign_profile
