8846fefd33bf98f87e6bca7311698301aba207e2
Tom Sabin <tom.sabin@unboxedconsulting.com>
2015-06-01 17:31:21 +0100

    Admin can CRUD a Host

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,4 +4,8 @@ class Profile < ActiveRecord::Base
   accepts_nested_attributes_for :user
 
   validates :user, :date_of_birth, :profession, :bio, :mobile_number, :favorite_cuisine, presence: true
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
