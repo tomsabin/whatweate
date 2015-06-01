48b20b44cf55bc9c522d4a7554b20393efb3ba19
Tom Sabin <tom.sabin@unboxedconsulting.com>
2015-06-01 13:59:28 +0100

    Admin can edit and destroy an Event

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -1,12 +1,12 @@
 class Admin::EventsController < ApplicationController
   # before_action :authenticate_admin!
+  before_action :assign_event, except: [:index, :new, :create]
 
   def index
     @events = Event.all
   end
 
   def show
-    @event = Event.find(params[:id])
   end
 
   def new
@@ -22,7 +22,31 @@ class Admin::EventsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @event.update(event_params)
+      redirect_to(admin_event_url(@event), notice: "Event successfully updated")
+    else
+      render(:edit)
+    end
+  end
+
+  def destroy
+    if @event.present?
+      @event.destroy
+      redirect_to(root_path, notice: "Event successfully deleted")
+    else
+      redirect_to(edit_admin_event_url(@event))
+    end
+  end
+
   private
+
+  def assign_event
+    @event = Event.find(params[:id])
+  end
 
   def event_params
     params.require(:event).permit(:title, :location, :description, :menu, :seats, :price_in_pennies, :currency)
