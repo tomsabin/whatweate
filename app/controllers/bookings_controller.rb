ce31a09f5edc39d43a19fbe6554ee33517aec0e1
Tom Sabin <tom.sabin@unboxedconsulting.com>
2015-06-03 23:12:24 +0100

    Extract booking creation to own service

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,26 +1,7 @@
 class BookingsController < ApplicationController
-  before_action :assign_event
-
   def create
-    if @event.sold_out?
-      flash[:notice] = "Sorry, this event has sold out"
-    elsif @event.seated?(current_user)
-      flash[:notice] = "You are already booked on this event"
-    elsif user_signed_in?
-      if EventBooking.make(event: @event, user: current_user)
-        flash[:notice] = "Thanks! We've booked you a seat"
-      else
-        flash[:notice] = t("error")
-      end
-    else
-      flash[:notice] = t("devise.failure.unauthenticated")
-    end
+    event = Event.find(params[:event_id])
+    flash[:notice] = CreateBooking.perform(event, current_user)
     redirect_to(event_url(params[:event_id]))
-  end
-
-  private
-
-  def assign_event
-    @event = Event.find(params[:event_id])
   end
 end
