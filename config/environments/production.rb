87e26563236ec236c58b3575c46018fefedd107c
Tom Sabin <tom.sabin@unboxedconsulting.com>
2015-06-04 13:44:42 +0100

    Use plain authentication method for ActionMailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,6 +20,6 @@ Rails.application.configure do
     domain:               "gmail.com",
     user_name:            ENV["MAILER_USER_NAME"],
     password:             ENV["MAILER_PASSWORD"],
-    authentication:       :login,
+    authentication:       :plain,
     enable_starttls_auto: true  }
 end
