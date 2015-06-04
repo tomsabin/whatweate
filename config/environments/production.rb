d1fa71055e1514b91716ab91aed478d46064e48e
Tom Sabin <tom.sabin@unboxedconsulting.com>
2015-06-04 11:07:32 +0100

    Add ActionMailer config with no-reply email address

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,4 +12,13 @@ Rails.application.configure do
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              "smtp.gmail.com",
+    port:                 587,
+    domain:               "gmail.com",
+    user_name:            ENV["MAILER_USER_NAME"],
+    password:             ENV["MAILER_PASSWORD"],
+    authentication:       :login,
+    enable_starttls_auto: true  }
 end
