34de60989ab2feeab09585eacd6c4aaaa0520cb1
Tom Sabin <tom.sabin@unboxedconsulting.com>
2015-06-04 15:20:18 +0100

    Production should not raise ActionMailer errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,6 +13,8 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.default_url_options = { :host => ENV["MAILER_HOST"] }
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              ENV["MAILER_ADDRESS"],
