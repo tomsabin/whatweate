c21bcc30208ac7b84dae0f78184fb7ea5d0965b8
Tom Sabin <tom.sabin@unboxedconsulting.com>
2015-06-04 09:09:35 +0100

    Fix path for Materialize font assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,3 +1,4 @@
 Rails.application.config.assets.version = "1.0"
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts')
-Rails.application.config.assets.precompile += %w( admin.css admin.js html5shiv.min.js *.svg *.eot *.woff *.ttf )
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts', 'materialize', 'material-design-icons')
+Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'fonts', 'materialize', 'roboto')
+Rails.application.config.assets.precompile += %w( admin.css admin.js html5shiv.min.js *.svg *.eot *.woff *.woff2 *.ttf )
