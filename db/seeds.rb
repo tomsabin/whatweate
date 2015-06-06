16879e275783be83275bdd3ef3b562f91a8fdab5
Tom Sabin <tom.sabin@unboxedconsulting.com>
2015-06-06 06:57:00 +0100

    Update seeds to be able to re-run

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,10 @@
 if Rails.env.development?
-  FactoryGirl.create(:user, email: "user@example.com", password: "letmein!!")
-  FactoryGirl.create(:admin, email: "admin@example.com", password: "letmein!!")
+  Admin.find_or_create_by(email: "admin@example.com")
+    .update(FactoryGirl.attributes_for(:admin).merge(password: "letmein!!").except(:email))
+  User.find_or_create_by(email: "user@example.com")
+    .update(FactoryGirl.attributes_for(:user).merge(password: "letmein!!").except(:email))
+  Host.find_or_create_by(name: "Event host")
+    .update(user: User.find_by(email: "user@example.com"))
+  Event.find_or_create_by(title: "Event title")
+    .update(FactoryGirl.attributes_for(:event).merge(host: Host.find_by(name: "Event host")).except(:title))
 end
