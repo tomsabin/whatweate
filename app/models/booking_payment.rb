83082869158e3b20ef8eb6f106287e69d81be219
Tom Sabin <tom.sabin@unboxedconsulting.com>
2015-06-09 14:17:49 +0100

    Charge the Guest and save their booking when making a booking payment

--- app/models/booking_payment.rb
+++ app/models/booking_payment.rb
@@ -1,16 +1,34 @@
 class BookingPayment
   include ActiveModel::Validations
-  attr_reader :booking, :payment
+  attr_reader :booking
 
   def initialize(booking, token)
     @booking = booking
     @token = token
+    @event = booking.event
+    @user = booking.user
   end
 
   def save
-    @payment = Payment.create(booking: booking, customer_reference: "customer", charge_reference: "charge")
+    customer = Stripe::Customer.create(
+      email: user.email,
+      card:  token
+    )
+
+    charge = Stripe::Charge.create(
+      customer:    customer.id,
+      amount:      event.price_in_pennies,
+      description: event.title,
+      currency:    event.currency
+    )
+
+    Payment.create(booking: booking, customer_reference: customer.id, charge_reference: charge.id)
+  rescue Stripe::CardError => e
+    errors.add(:base, e.message)
+  rescue => e
+    errors.add(:base, I18n.t("failure.generic"))
   end
 
   private
-  attr_reader :token
+  attr_reader :token, :event, :user
 end
--- spec/models/booking_payment_spec.rb
+++ spec/models/booking_payment_spec.rb
@@ -1,44 +1,93 @@
 require "rails_helper"
 
 describe BookingPayment do
-  subject { described_class.new(booking, token) }
-  let(:booking) { Booking.new(user: FactoryGirl.create(:user), event: FactoryGirl.create(:event)) }
+  let(:event) { FactoryGirl.create(:event, price_in_pennies: 1000, title: "Event title") }
+  let(:booking) { Booking.new(user: FactoryGirl.create(:user), event: event) }
+  let(:subject) { described_class.new(booking, token) }
 
-  context "valid details" do
-    let(:token) { "valid-token" }
+  describe "charging the user" do
+    let(:token) { "tok_16BmGWBb51nepfzbz2jrNiaX" }
 
-    it "saves the Payment" do
-      subject.save
-      payment = Payment.last
-      expect(payment).to be_present
-      expect(payment.customer_reference).to eq "customer"
-      expect(payment.charge_reference).to eq "charge"
-      expect(payment.booking).to eq Booking.last
-    end
-
-    it "saves the Booking" do
-      subject.save
-      booking = Booking.last
-      expect(booking).to be_present
-      expect(booking.payment).to eq Payment.last
-      expect(booking.event).to eq Event.last
-      expect(booking.user).to eq User.last
-    end
-
-    it "does not have any errors" do
-      subject.save
-      expect(subject.errors).to be_blank
-      expect(subject.errors.full_messages).to be_blank
+    it "is the correct amount" do
+      expect(Stripe::Charge).to receive(:create).with(hash_including(amount: 1000, currency: "GBP", description: "Event title"))
+      VCR.use_cassette("stripe/valid_card") { subject.save }
     end
   end
 
-  context "incorrect card number"
-  context "invalid card number"
-  context "invalid expiry month"
-  context "invalid expiry year"
-  context "invalid cvc"
-  context "expired card"
-  context "incorrect cvc"
-  context "processing error"
-  context "API rate limit"
+  context "tokens from Checkout" do
+    before { VCR.use_cassette(cassette) { subject.save } }
+
+    context "token validated by Checkout and can be charged" do
+      let(:token) { "tok_16BmGWBb51nepfzbz2jrNiaX" }
+      let(:cassette) { "stripe/valid_card" }
+
+      it "saves the Payment" do
+        payment = Payment.last
+        expect(payment).to be_present
+        expect(payment.customer_reference).to eq "cus_6OZPJHyHVOyNNS"
+        expect(payment.charge_reference).to eq "ch_16BmGZBb51nepfzbEhTpAalj"
+        expect(payment.booking).to eq Booking.last
+      end
+
+      it "saves the Booking" do
+        booking = Booking.last
+        expect(booking).to be_present
+        expect(booking.payment).to eq Payment.last
+        expect(booking.event).to eq Event.last
+        expect(booking.user).to eq User.last
+      end
+
+      it "does not have any errors" do
+        expect(subject.errors.full_messages).to be_blank
+      end
+    end
+
+    context "token validated by Checkout but cannot be charged" do
+      let(:token) { "tok_16BmHrBb51nepfzb6MnsBrlq" }
+      let(:cassette) { "stripe/card_error" }
+
+      it "does not save a Payment" do
+        expect(Payment.count).to eq(0)
+      end
+
+      it "does not save a Booking" do
+        expect(Booking.count).to eq(0)
+      end
+
+      it "has errors from Stripe" do
+        expect(subject.errors.full_messages).to eq ["Your card was declined."]
+      end
+    end
+
+    context "token validated by Checkout but card was declined" do
+      let(:token) { "tok_16BmIXBb51nepfzbkWvO7yhx" }
+      let(:cassette) { "stripe/card_declined" }
+
+      it "does not save a Payment" do
+        expect(Payment.count).to eq(0)
+      end
+
+      it "does not save a Booking" do
+        expect(Booking.count).to eq(0)
+      end
+
+      it "has errors from Stripe" do
+        expect(subject.errors.full_messages).to eq ["Your card was declined."]
+      end
+    end
+
+    context "something else goes wrong" do
+      let(:token) { "tok_16BmGWBb51nepfzbz2jrNiaX" }
+      let(:cassette) { "stripe/valid_card" }
+
+      before do
+        allow(Stripe::Customer).to receive(:create).and_raise(Stripe::StripeError.new)
+        VCR.use_cassette(cassette) { subject.save }
+      end
+
+      it "has generic errors" do
+        expect(subject.errors.full_messages).to eq ["Oops, something went wrong."]
+      end
+    end
+  end
 end
