4f0f1efc5efc1e420a09cc3884be102a814256be
Tom Sabin <tom.sabin@unboxedconsulting.com>
2015-06-09 16:33:13 +0100

    Move inclusion of helpers to the files themselves

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -22,6 +22,4 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
 
   config.include Devise::TestHelpers, type: :controller
-  config.include ControllerHelpers, type: :controller
-  config.include FeatureHelpers, type: :feature
 end
