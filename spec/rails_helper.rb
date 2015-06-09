15e378f97def4e35219feaa18be27a22113c9807
Tom Sabin <tom.sabin@unboxedconsulting.com>
2015-06-09 17:02:41 +0100

    Skip smoke tests for main RSpec run

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -20,6 +20,7 @@ RSpec.configure do |config|
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
   config.use_transactional_fixtures = false
   config.infer_spec_type_from_file_location!
+  config.filter_run_excluding :smoke
 
   config.include Devise::TestHelpers, type: :controller
 
