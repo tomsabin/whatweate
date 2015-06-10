db89c46ae0ba62f407906ef5371dbd88434d3378
Tom Sabin <tom.sabin@unboxedconsulting.com>
2015-06-10 14:15:41 +0100

    Expand controller specs to assert instance variable assignment logic

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -3,8 +3,8 @@ class MembersController < ApplicationController
     @user = User.friendly.find(params[:id]).decorate
     return redirect_to(action: :not_found, controller: :errors) unless @user.completed_profile?
 
-    @verified_with_facebook = Identity.facebook(@user)
-    @verified_with_twitter = Identity.twitter(@user)
+    @verified_with_facebook = Identity.facebook?(@user)
+    @verified_with_twitter = Identity.twitter?(@user)
 
     @upcoming_booked_events = @user.booked_events.upcoming
     @past_booked_events = @user.booked_events.past
--- spec/models/identity_spec.rb
+++ spec/models/identity_spec.rb
@@ -8,4 +8,30 @@ describe Identity do
     it { should validate_presence_of(:provider) }
     it { should validate_uniqueness_of(:uid).scoped_to(:provider) }
   end
+
+  describe ".facebook?" do
+    let(:user) { FactoryGirl.create(:user, :facebook) }
+    it { expect(described_class.facebook?(user)).to eq true }
+  end
+
+  describe ".facebook" do
+    let(:user) { FactoryGirl.create(:user, :facebook) }
+
+    it "returns the identity" do
+      expect(described_class.facebook(user)).to eq Identity.last
+    end
+  end
+
+  describe ".twitter?" do
+    let(:user) { FactoryGirl.create(:user, :twitter) }
+    it { expect(described_class.twitter?(user)).to eq true }
+  end
+
+  describe ".twitter" do
+    let(:user) { FactoryGirl.create(:user, :twitter) }
+
+    it "returns the identity" do
+      expect(described_class.twitter(user)).to eq Identity.last
+    end
+  end
 end
