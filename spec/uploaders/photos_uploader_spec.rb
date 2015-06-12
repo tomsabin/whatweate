c27961bb6c91817da96fabd848a1351c6b5a0191
Tom Sabin <tom.sabin@unboxedconsulting.com>
2015-06-12 14:09:46 +0100

    Don't use instance variables in before(:all) block, produces flaky tests

--- spec/uploaders/photos_uploader_spec.rb
+++ spec/uploaders/photos_uploader_spec.rb
@@ -2,40 +2,43 @@ require "rails_helper"
 
 describe PhotosUploader, type: :uploader do
   let(:path_to_image) { Rails.root.join("fixtures", "carrierwave", "image.png") }
+  let(:event) { FactoryGirl.create(:event) }
 
-  before(:all) do
+  before do
     described_class.enable_processing = true
-    @event = FactoryGirl.create(:event)
-    @event.photos = [
+    event.photos = [
       File.open(Rails.root.join("fixtures", "carrierwave", "image.png")),
       File.open(Rails.root.join("fixtures", "carrierwave", "image.png"))
     ]
-    @event.save
+    event.save
   end
 
-  after(:all) { @event.remove_photos! }
+  after do
+    described_class.enable_processing = false
+    event.remove_photos!
+  end
 
   describe "multiple files" do
-    it { expect(@event.photos.size).to eq 2 }
+    it { expect(event.photos.size).to eq 2 }
   end
 
   context "the full version" do
     it "should scale down a landscape image to be exactly 1024 by 678 pixels" do
-      expect(@event.photos.first).to have_dimensions(1024, 678)
+      expect(event.photos.first).to have_dimensions(1024, 678)
     end
 
     it "has the correct url" do
-      expect(@event.photos.first.url).to match /uploads\/events\/(\d)+\/photos\/\w{8}(-\w{4}){3}-\w{12}\.png/
+      expect(event.photos.first.url).to match /uploads\/events\/(\d)+\/photos\/\w{8}(-\w{4}){3}-\w{12}\.png/
     end
   end
 
   context "the thumb version" do
     it "should scale down a landscape image to fit within 292 by 194 pixels" do
-      expect(@event.photos.first.thumb).to be_no_larger_than(292, 194)
+      expect(event.photos.first.thumb).to be_no_larger_than(292, 194)
     end
 
     it "has the correct url" do
-      expect(@event.photos.first.thumb.url).to match /uploads\/events\/(\d)+\/photos\/thumb_\w{8}(-\w{4}){3}-\w{12}\.png/
+      expect(event.photos.first.thumb.url).to match /uploads\/events\/(\d)+\/photos\/thumb_\w{8}(-\w{4}){3}-\w{12}\.png/
     end
   end
 end
