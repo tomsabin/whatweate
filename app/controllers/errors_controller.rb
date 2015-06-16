6d3658e4e66171a2bd5de8d58769363378cc7cf3
Tom Sabin <tom.sabin@unboxedconsulting.com>
2015-06-16 13:46:49 +0100

    Fix custom internal server error from being loaded

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -3,7 +3,11 @@ class ErrorsController < ApplicationController
     render status: :not_found
   end
 
-  def internal_error
+  def unprocessable_entity
+    render status: :unprocessable_entity
+  end
+
+  def internal_server_error
     render status: :internal_server_error
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -34,6 +34,7 @@ Rails.application.routes.draw do
 
   root "pages#home"
 
-  get "/404", to: "errors#not_found"
-  get "/500", to: "errors#internal_error"
+  match "/404", to: "errors#not_found", via: :all
+  match "/422", to: "errors#unprocessable_entity", via: :all
+  match "/500", to: "errors#internal_server_error", via: :all
 end
