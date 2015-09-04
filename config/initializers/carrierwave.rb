bfa6ded90f80788beea0cc11edaa465ab40ef001
Tom Sabin <tom.sabin@unboxedconsulting.com>
2015-09-04 11:37:08 +0100

    Configure AWS S3 photo storage for production environments

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,5 +1,19 @@
 CarrierWave.configure do |config|
-  config.permissions = 0644
-  config.storage = :file
-  config.root = Rails.root.join("tmp") if Rails.env.test?
+  if Rails.env.test?
+    config.permissions = 0644
+    config.storage     = :file
+    config.root        = Rails.root.join("tmp")
+  elsif Rails.env.development?
+    config.permissions = 0644
+    config.storage     = :file
+  else
+    config.storage    = :aws
+    config.aws_bucket = ENV["AWS_S3_BUCKET_NAME"]
+    config.aws_acl    = 'public-read'
+    config.aws_credentials = {
+      access_key_id:     ENV["AWS_ACCESS_KEY_ID"],
+      secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"],
+      region:            ENV["AWS_REGION"]
+    }
+  end
 end
